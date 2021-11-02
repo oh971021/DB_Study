070c8a143eb055f187147271b642dd1c908d9e2b
Junseok <oh971021@gmail.com>
2021-11-02 15:41:51 +0900

    1102 DB Study : CRUD ( Update & Delete )

--- 06_JDBC_CRUD/bin/RDB.sql
+++ 06_JDBC_CRUD/bin/RDB.sql
@@ -32,10 +32,10 @@ create table owner(
 	o_restaurant number(3) not null
 );
 
-create sequence menu_seq;
-create sequence restaurant_seq;
-create sequence human_seq;
-create sequence owner_seq;
+create sequence menu_seq increment by 1 start with 1;
+create sequence restaurant_seq increment by 1 start with 1;
+create sequence human_seq increment by 1 start with 1;
+create sequence owner_seq increment by 1 start with 1;
 
 insert into menu values (menu_seq.nextval, '돼지곱창', 10000, 1);
 insert into menu values (menu_seq.nextval, '소곱창', 15000, 1);
@@ -75,6 +75,273 @@ insert into owner values (owner_seq.nextval, 2, 2);
 insert into owner values (owner_seq.nextval, 3, 3);
 insert into owner values (owner_seq.nextval, 4, 4);
 -- insert into owner values (owner_seq.nextval, 200, 4);
+
+-- 이게 무슨 의미
+insert into owner values (owner_seq.nextval, 4, 500);
+
+-- 조회하기
+select * from menu;
+select * from restaurant;
+select * from human;
+select * from owner;
+
+-- 조건 조회 ( 내가 알고있는 맛집 이름, 위치(매장 조회) - 이름 가나다순
+select r_name, r_place from restaurant order by r_name;
+
+-- 조건 조회 ( 메뉴 중 제일 비싼 메뉴의 정보 ) : subQuery
+select * from menu where m_price = (
+	select max(m_price) from menu
+);
+
+-- 최 연장자 정보
+select * from human where h_birth = (
+	select min(h_birth) from human
+);
+
+-- 곱창 시리즈 평균가
+select avg(m_price) from menu
+where m_name like '%곱창%';
+
+-- 종로 가게 정보
+select * from RESTAURANT where r_place = '종로';
+
+-- 제일 싼 메뉴를 파는 식당의 이름과 위치 ( 이름이 다른 테이블을 연결 시키기 )
+	-- subQuery 는 하나 이상의 결과가 나오면 안된다.
+		-- 그때는 in 키워드를 사용한다.
+select r_name, r_place from RESTAURANT where r_no in (
+	select m_place from MENU where m_price = (
+		select min(m_price) from menu
+	)
+);
+
+-- 동대문에서 먹을 수 있는 음식의 이름과 가격
+select m_name, m_price 
+from menu 
+where m_place in (
+	select r_no 
+	from RESTAURANT 
+	where r_place = '동대문'
+);
+
+-- 곱창 시리즈는 어느 지역에 가면 먹을 수 있나요?
+select r_place, r_name 
+from RESTAURANT
+where r_no in (
+	select m_place
+	from menu
+	where m_name like '%곱창%'
+);
+
+-- 제일 싼 커피를 파는 매장의 이름과 지역
+select r_place, r_name
+from restaurant
+where r_no in (
+	select m_place
+	from menu 
+	where m_price = (
+		select min(m_price)
+		from menu 
+		where (m_name like '%라떼%' or m_name like '%카노%')
+	)
+);
+
+----------------------------------------------------------------
+
+-- JOIN : 서로 다른 테이블 연결하는 거 pk를 통해서
+
+-- 두개 테이블 연결해서 출력하기
+select m_name, m_price, r_name, r_place
+from MENU, RESTAURANT
+where m_place = r_no
+
+-- 스타벅스 종로지점에서 판매하는 메뉴 이름과 가격
+	-- 테이블 관계명시가 중요하다
+-- 1번 방법
+select r_name, r_place, m_name, m_price
+from RESTAURANT, menu
+where r_name = '스타벅스' and r_place = '종로' and r_no = m_place;
+
+-- 2번 방법
+select r_name, r_place, m_name, m_price
+from RESTAURANT, MENU
+where r_no = m_place and m_place = (
+	select r_no
+	from RESTAURANT
+	where r_name = '스타벅스'
+	and r_place = '종로'
+);
+
+-- 제일 젊은 사장님의 가게의 매장명, 위치, 사장님 이름, 생일, 메뉴명, 가격
+select h_name, h_birth, r_name, r_place, m_name, m_price
+from human, restaurant, menu, owner
+where r_no = m_place 
+	and h_no = o_ceo
+	and o_restaurant = r_no
+	and h_birth = (
+		select max(h_birth)
+		from human
+		)
+
+-- 최연장자 아저씨네 메뉴명, 메뉴 가격,
+
+-- 1번 방법 ( RDB )
+select h_name, m_name, m_price
+from human, owner, RESTAURANT, menu
+where r_no = m_place 
+	and h_no = o_ceo
+	and o_restaurant = r_no
+	and h_birth = (
+		select min(h_birth)
+		from human
+		);
+
+-- 2번 방법 ( RDB )
+select h_name, m_name, m_price
+from human, OWNER, menu
+where h_no = o_ceo
+	and o_restaurant = m_place
+	and h_birth = (
+		select min(h_birth)
+		from human
+		)
+		
+-- 3번 방법 ( subQuery )
+select m_name, m_price
+from menu
+where m_place in (
+	select r_no
+	from RESTAURANT
+	where r_no = (
+		select o_ceo
+		from owner
+		where o_ceo = (
+			select h_no
+			from HUMAN
+				where h_birth =(
+				select min(h_birth)
+				from human
+				)
+			)
+		)
+	);
+
+-- 4번 방법 ( subQuery )
+select m_name, m_price
+from menu
+where m_place = (
+		select o_restaurant
+		from owner
+		where o_ceo = (
+			select h_num
+			from human
+			where h_birth = (
+				select min(h_birth)
+				from human
+			)
+		)
+	);
+
+----------------------- TEST ------------------------
+
+-- 황소곱창 가게의 전체 메뉴명, 가격, 매장 위치
+select m_name, m_price, r_place
+from MENU, restaurant
+where m_place = r_no
+	and r_name = '황소곱창';
+	
+-- 제일 싼거 파는 매장명, 위치, 메뉴명, 가격
+select r_name, r_place, m_name, m_price
+from restaurant, menu
+where r_no = m_place; 
+	and r_no = (
+		select m_place
+		from menu
+		where m_price = (
+			select min(m_price)
+			from menu
+			)
+		)
+	
+------------------------------------------------------
+
+-- CR ( Update, Delete )
+		
+-- 돼지곱창 가격 인상 (13000원)
+update menu 
+set m_price = 13000 
+where m_name = '돼지곱창';
+
+-- 젤 싼 메뉴 무료 이벤트
+update menu 
+set m_price = 0 
+where m_price = (
+		select min(m_price)
+		from menu
+	);
+		
+-- 메뉴 전체의 평균보다 비싼 메뉴를 10% 할인
+update menu
+set m_price = m_price - (m_price/10)
+where m_price > (
+	select avg(m_price)
+	from menu
+	);
+		
+-- 동대문 지역 메뉴들 1000원 인상
+update menu
+set m_price = m_price + 1000
+where m_place in (
+	select r_no
+	from restaurant
+	where r_place = '동대문'
+	);
+		
+-- 커피 중 '라떼' 종류의 가격 500원 인상
+update menu
+set m_price = m_price + 500
+where m_name like '%라떼%';
+		
+select * from menu;
+
+----------------------------------------------------
+
+-- CR ( Update, Delete )
+
+delete menu where m_name = '딸기스무디';
+
+-- '라떼' 글자가 들어간 메뉴 다 삭제
+delete menu 
+where m_name like '%라떼%';
+
+-- 강남 스벅 (메뉴 삭제)
+delete menu
+where m_place = (
+	select r_no
+	from restaurant
+	where r_name = '스타벅스' and r_place = '강남'
+	);
+
+-- 강남 스벅 (폐점)
+delete restaurant
+where r_name = '스타벅스' and r_place = '강남';
+
+-- '곱창' 들어간 매장의 메뉴 다 삭제
+delete menu
+where m_place in (
+	select r_no
+	from restaurant
+	where r_name like '%곱창%'
+	);
+
+select * from menu;
+
+select * from restaurant;
+
+select m_name, r_name 
+from menu, restaurant
+where r_no = m_place
+	and r_name like '%곱창%';
+
 
 -------------------------------------------------드랍
 
