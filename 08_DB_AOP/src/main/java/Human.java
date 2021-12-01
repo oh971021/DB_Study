cb8546bbe269fc6afc113021cde819a7c6c10b4e
Junseok <oh971021@gmail.com>
2021-12-01 22:39:47 +0900

    1201 DB Study : JSTL (Aspect Oriented Programming)

---
+++ 08_DB_AOP/src/main/java/Human.java
@@ -0,0 +1,22 @@
+public class Human {
+	
+	public void ready() {
+		System.out.println("Àá¿¡¼­ ±ú¼­");
+		System.out.println("¾Ä±â");
+		System.out.println("¿ÊÀÔ±â");
+		System.out.println("½Å¹ß½Å±â");
+	}
+	
+	public void goAcademy() {
+		ready();
+		System.out.println("¹ö½º");
+		System.out.println("ÇĞ¿ø µµÂø");
+	}
+	
+	public void goSchool() {
+		ready();
+		System.out.println("°È±â");
+		System.out.println("ÇĞ±³ µµÂø");
+	}
+
+}
