cab8293116848f14dfdca47e95d5c40d37d17f64
Junseok <oh971021@gmail.com>
2021-12-02 13:00:52 +0900

    1202 JSTL Study : Login System (connect DB)

---
+++ 11_LoginSystem/src/main/java/com/js/main/LoginController.java
@@ -0,0 +1,26 @@
+package com.js.main;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/LoginController")
+public class LoginController extends HttpServlet {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		// ¿œ
+		
+		
+		request.setAttribute("loginPage", "loginOK.jsp");
+		request.setAttribute("contentPage", "home.jsp");
+		request.getRequestDispatcher("index.jsp").forward(request, response);
+	}
+
+}
---
+++ 11_LoginSystem/src/main/java/com/js/menu/M2Controller.java
@@ -0,0 +1,20 @@
+package com.js.menu;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/M2Controller")
+public class M2Controller extends HttpServlet {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	}
+
+}
